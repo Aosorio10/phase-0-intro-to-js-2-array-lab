9400163b720acdf9771ef2eea6e3aa96b6f3d6ea
Alejandro Osorio <avot3110@hotmail.com>
2023-04-05 13:26:18 -0400

    Arrays Lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = function(){
+    return cats.push("Ralph");
+}
+const destructivelyPrependCat = function (){
+    return cats.unshift("Bob");
+}
+
+const destructivelyRemoveLastCat = function(){
+    return cats.pop();
+}
+ const destructivelyRemoveFirstCat = function(){
+    return cats.shift();
+ }
+
+const appendCat = function (){
+    return[...cats,'Broom']
+}
+
+const prependCat = function (){
+    return["Arnold", ...cats]
+}
+const removeLastCat = function(){
+    return cats.slice(0,2);
+}
+const removeFirstCat = function(){
+    return cats.slice(-2);
+}
